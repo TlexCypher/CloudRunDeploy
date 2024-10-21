898e4aed63cbe6433b8621b00bf5cbe4446bf339
Taichi Araki <araki130710@gmail.com>
2024-10-21 14:07:21 +0900

    add test-build workflow and deploy-workflow

---
+++ main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	recorder := httptest.NewRecorder()
+
+	req := httptest.NewRequest("GET", "/", nil)
+
+	Handler(recorder, req)
+
+	if status := recorder.Code; status != http.StatusOK {
+		t.Errorf("expected status code %v, got %v", http.StatusOK, status)
+	}
+
+	expectedBody := "Hello go http server."
+	if strings.TrimSpace(recorder.Body.String()) != expectedBody {
+		t.Errorf("expected body '%v', got '%v'", expectedBody, recorder.Body.String())
+	}
+}
