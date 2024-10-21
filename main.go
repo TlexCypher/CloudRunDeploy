2516654d959703f8b0efaca87c9ae3a6e3ac5ab3
Taichi Araki <araki130710@gmail.com>
2024-10-21 15:52:30 +0900

    add secrets and workload identity settings.

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func Handler(w http.ResponseWriter, req *http.Request) {
 	log.Println("Hello go http server.")
-	msg := "Hello go http server."
+	msg := "Success to start go http server and auto-deploy to Cloud Run."
 	_, err := w.Write([]byte(msg))
 	if err != nil {
 		log.Fatalf("Failed to start go http server.")
